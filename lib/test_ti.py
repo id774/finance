26c1577bfca776d70896a8c22a105b5b1c3f6c60
id774 <idnanashi@gmail.com>
2015-03-24 15:28:08 +0900

    9 Technical Indicators using TA-Lib

--- lib/test_ti.py
+++ lib/test_ti.py
@@ -1,42 +1,41 @@
 from nose.tools import *
 from ti import *
+import os
+import pandas as pd
+
+def testdata():
+    days = 30
+    filename = os.path.join(os.path.dirname(
+                            os.path.abspath(__file__)),
+                            'stock_N225.csv')
+    stock_tse = pd.read_csv(filename,
+                            index_col=0, parse_dates=True)
+    return stock_tse.asfreq('B')[days:]
+
+def test_get_prices():
+    stock = testdata()
+    ti = TechnicalIndicators(stock)
+    prices = ti.get_prices()
+
+    expected = 19560.22
+    result = prices.ix['2015-03-20', 'prices']
+    eq_(expected, result)
+    return prices
 
 def test_get_macd():
-    ti = TechnicalIndicators()
-    ema_12 = 29.24
-    ema_26 = 28.21
-    macd = ti.get_macd(ema_12, ema_26)
-    eq_(str(round(macd, 2)), "1.03")
+    stock = testdata()
+    ti = TechnicalIndicators(stock)
+    macd = ti.get_macd()
+
+    expected = (382.99041705210402,
+                345.64437737584518,
+                37.34603967625884)
+    result = (macd.ix['2015-03-20', 'macd'],
+              macd.ix['2015-03-20', 'macdsignal'],
+              macd.ix['2015-03-20', 'macdhist'])
+    eq_(expected, result)
     return macd
 
-def test_get_macd_ema():
-    ti = TechnicalIndicators()
-    macd_12_26_arr = [64.75, 63.79, 63.73, 63.73, 63.55, 63.19, 63.91, 63.85,
-                      62.95, 63.37, 61.33, 61.51, 61.87, 60.25, 59.35, 59.95,
-                      58.93, 57.68, 58.82, 58.87]
-    macd_arr = []
-    for macd in macd_12_26_arr:
-        sp = StockPrice()
-        sp.macd_12_26 = macd
-        macd_arr.append(sp)
-    signals = []
-    prev_signal = 0
-    for x, m in enumerate(macd_arr):
-        tmp_signal = ti.get_macd_ema(macd_arr, x, 9, prev_signal)
-        prev_signal = tmp_signal
-        signals.append(tmp_signal)
-    eq_(str(round(signals[14], 3)), "61.608")
-    return signals
-
-def test_get_macd_histogram():
-    ti = TechnicalIndicators()
-    macd = 1.01
-    ema_9 = 0.88
-    macd_hist = ti.get_macd_histogram(macd, ema_9)
-    eq_(str(round(macd_hist, 2)), "0.13")
-    return macd_hist
-
 if __name__ == '__main__':
+    print(test_get_prices())
     print(test_get_macd())
-    print(test_get_macd_ema())
-    print(test_get_macd_histogram())
--- lib/ti.py
+++ lib/ti.py
@@ -1,37 +1,22 @@
+import numpy as np
+import pandas as pd
+import talib as ta
+
 class TechnicalIndicators():
 
-    def get_macd(self, ema_12, ema_26):
-        return (float(ema_12) - float(ema_26))
+    def __init__(self, stock, **kwargs):
+        stock = stock.asfreq('B')['Adj Close'].dropna()
+        self.prices = np.array(stock)
+        self.analysis = pd.DataFrame(index=stock.index.values)
 
-    def get_macd_ema(self, stock_prices_array, idx, period, previous_macd_ema):
-        smoothing_constant = 2.0 / (period + 1)
-        ma_array = []
-        if (idx == (period - 1)):
-            start_ma_array = idx - (period - 1)
-            end_ma_array = start_ma_array + period
-            ma_array = stock_prices_array[start_ma_array:end_ma_array]
-            ma = (sum(x.macd_12_26 for x in ma_array) / period)
-            return ma
-        elif (idx > (period - 1) and previous_macd_ema != None):
-            current_macd = stock_prices_array[idx].macd_12_26
-            ema = ((smoothing_constant * (current_macd - previous_macd_ema)) +
-                   previous_macd_ema)
-            return ema
-        else:
-            return 0
+    def get_prices(self):
+        self.analysis['prices'] = self.prices
+        return self.analysis
 
-    def get_macd_histogram(self, macd, ema_9):
-        return (float(macd) - float(ema_9))
-
-
-# This is the StockPrice object in the method above, for reference
-class StockPrice:
-    id = 0
-    symbol = ""
-    volume = 0
-    opening_price = 0
-    closing_price = 0
-    adj_closing_price = 0
-    low_price = 0
-    high_price = 0
-    macd_12_26 = 0
+    def get_macd(self):
+        macd, macdsignal, macdhist = ta.MACD(
+            self.prices, fastperiod=12, slowperiod=26, signalperiod=9)
+        self.analysis['macd'] = macd
+        self.analysis['macdsignal'] = macdsignal
+        self.analysis['macdhist'] = macdhist
+        return self.analysis
