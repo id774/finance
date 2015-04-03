91c8691c228f1dac5892e761240563876cbe93a2
id774 <idnanashi@gmail.com>
2015-04-03 11:30:27 +0900

    59 Append tests of FileIO

--- test/test_file_io.py
+++ test/test_file_io.py
@@ -36,5 +36,15 @@ def test_save_data():
     if os.path.exists(filename):
         os.remove(filename)
 
+def test_read_csv():
+    io = FileIO()
+    filename = os.path.join(os.path.dirname(
+                            os.path.abspath(__file__)),
+                            'stock_N225.csv')
+    df = io.read_from_csv("N225", filename)
+    result = round(df.ix['2015-03-20', 'Adj Close'], 2)
+    expected = 19560.22
+    eq_(expected, result)
+
 if __name__ == '__main__':
     stock = testdata()
