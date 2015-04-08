2b4c762d65a088a029c4ae53fe5b654e80b14550
id774 <idnanashi@gmail.com>
2015-04-08 16:05:16 +0900

    79 Improvement plotting with a single picture

--- test/test_draw.py
+++ test/test_draw.py
@@ -21,29 +21,12 @@ def testdata():
                             index_col=0, parse_dates=True)
     return stock_tse.asfreq('B')[days:]
 
-def test_plot_ohlc():
+def plot_ohlc():
     stock = testdata()
     draw = Draw("N225", "日経平均株価")
 
     ti = TechnicalIndicators(stock)
-    ewma = ti.calc_ewma(span=5)
-    ewma = ti.calc_ewma(span=25)
-    ewma = ti.calc_ewma(span=75)
-    bbands = ti.calc_bbands()
-    draw.plot_ohlc(stock, ewma, bbands)
 
-    filename = 'ohlc_N225.png'
-    expected = True
-    eq_(expected, os.path.exists(filename))
-
-    if os.path.exists(filename):
-        os.remove(filename)
-
-def test_plot_osci():
-    stock = testdata()
-    draw = Draw("N225", "日経平均株価")
-
-    ti = TechnicalIndicators(stock)
     ret = ti.calc_ret_index()
     rsi = ti.calc_rsi(timeperiod=9)
     rsi = ti.calc_rsi(timeperiod=14)
@@ -52,10 +35,17 @@ def test_plot_osci():
     stoch = ti.calc_stoch()
     stochf = ti.calc_stochf()
     vr = ti.calc_volume_ratio()
-    draw.plot_osci(ret, rsi, mfi, ultosc,
-                   stoch, stochf, vr)
 
-    filename = 'osci_N225.png'
+    ewma = ti.calc_ewma(span=5)
+    ewma = ti.calc_ewma(span=25)
+    ewma = ti.calc_ewma(span=75)
+    bbands = ti.calc_bbands()
+
+    draw.plot(stock, ewma, bbands,
+              ret, rsi, mfi, ultosc,
+              stoch, stochf, vr)
+
+    filename = 'chart_N225.png'
     expected = True
     eq_(expected, os.path.exists(filename))
 
