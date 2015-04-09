79e83833caa779ab509f1a1e1d36ee11ec2b050a
id774 <idnanashi@gmail.com>
2015-04-09 14:14:42 +0900

    86 Append demo code

--- demo.py
+++ demo.py
@@ -7,6 +7,8 @@ if p in sys.path:
 else:
     sys.path.append(p)
 from analysis import Analysis
+from file_io import FileIO
+from ti import TechnicalIndicators
 
 def demo(stock='N225',
          name='日経平均株価',
@@ -18,6 +20,18 @@ def demo(stock='N225',
              'stock_N225.csv'),
          update=False):
 
+    # Handling ti object example.
+    io = FileIO()
+    stock_d = io.read_from_csv(stock,
+                               csvfile)
+    ti = TechnicalIndicators(stock_d)
+    ti.calc_ret_index()
+
+    print(ti.stock['ret_index'].tail(10))
+    io.save_data(io.merge_df(stock_d, ti.stock),
+                 stock, 'demo_')
+
+    # Run analysis code example.
     analysis = Analysis(stock=stock,
                         name=name,
                         start=start,
@@ -25,3 +39,15 @@ def demo(stock='N225',
                         csvfile=csvfile,
                         update=True)
     return analysis.run()
+
+if __name__ == '__main__':
+    argsmin = 0
+    version = (3, 0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            demo()
+        else:
+            print("This program needs at least %(argsmin)s arguments" %
+                  locals())
+    else:
+        print("This program requires python > %(version)s" % locals())
