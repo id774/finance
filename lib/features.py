2060083d7a9062183ddd82f6c1267c86d5c5c610
id774 <idnanashi@gmail.com>
2015-04-09 14:55:59 +0900

    87 Add Features Class

---
+++ lib/features.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import numpy as np
+p = os.path.dirname(os.path.abspath(__file__))
+if p in sys.path:
+    pass
+else:
+    sys.path.append(p)
+
+class Features():
+
+    def up_down_index(self, arr):
+        train_X = []
+        train_y = []
+        for i in np.arange(-90, -15):
+            s = i + 14
+            feature = arr.ix[i:s]
+            if feature[-1] < arr[s]:
+                train_y.append(1)
+            else:
+                train_y.append(0)
+            train_X.append(feature.values)
+        return np.array(train_X), np.array(train_y)
