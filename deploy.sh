3343db1b9c86be6e839f5fc483dd2a2c257d2567
id774 <idnanashi@gmail.com>
2015-04-12 09:23:08 +0900

    99 Update deploy script for classifier

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ sudo cp -av charts.py $TARGET_DIR
 sudo cp -av lib $TARGET_DIR
 sudo chmod 750 $TARGET_DIR
 test -d $TARGET_DIR/data || sudo mkdir -p $TARGET_DIR/data
+test -d $TARGET_DIR/clf || sudo mkdir -p $TARGET_DIR/clf
 sudo cp -av run.sh $TARGET_DIR/
 sudo chmod 750 $TARGET_DIR/run.sh
 sudo chown -R root:adm $TARGET_DIR
