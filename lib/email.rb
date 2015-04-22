502429f5542dd6f20c117487bba014c762ca8c46
id774 <idnanashi@gmail.com>
2015-04-22 10:36:57 +0900

    109 Summary report mail

--- lib/email.rb
+++ lib/email.rb
@@ -1,27 +1,37 @@
 require 'mail'
 
-options = {
-  :address  => "localhost",
-  :port   => 25,
-  :authentication => 'plain',
-  :enable_starttls_auto => true
-}
+def sendmail
+  options = {
+    :address  => "localhost",
+    :port   => 25,
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
-Mail.defaults do
-  delivery_method :smtp, options
+  Mail.defaults do
+    delivery_method :smtp, options
+  end
+
+  filename = "summary.csv"
+  dir = File.expand_path(File.dirname(__FILE__))
+  path = File.join(dir, "..", "data", filename)
+
+  mail = Mail.new do
+    from     "finance@id774.net"
+    to       "774@id774.net"
+    subject  "Summary Report of Financial Data"
+    body     File.read(path)
+  end
+
+  mail.charset = 'utf-8'
+  mail.delivery_method :sendmail
+  mail.deliver!
 end
 
-filename = "summary.csv"
-dir = File.expand_path(File.dirname(__FILE__))
-path = File.join(dir, "..", "data", filename)
-
-mail = Mail.new do
-  from     "finance@id774.net"
-  to       "774@id774.net"
-  subject  "Summary of Finance Data"
-  body     File.read(path)
+if __FILE__ == $0
+  hostname = `hostname`.chop
+  if hostname.include?("id774.net")
+    puts "Sending Summary Report from id774.net"
+    sendmail
+  end
 end
-
-mail.charset = 'utf-8'
-mail.delivery_method :sendmail
-mail.deliver!
