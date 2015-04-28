87f84c4529cc0a51aaa1353d4db2c1f9ff480ce8
id774 <idnanashi@gmail.com>
2015-04-28 11:09:44 +0900

    123 Fix dir

--- bin/summary.py
+++ bin/summary.py
@@ -1,7 +1,7 @@
 import sys
 import os
 p = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '..', '..', 'lib')
+    os.path.dirname(os.path.abspath(__file__)), '..', 'lib')
 if p in sys.path:
     pass
 else:
@@ -12,7 +12,7 @@ def main():
     aggregator = Aggregator()
     result = aggregator.summarize()
     p = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'data',
+        os.path.dirname(os.path.abspath(__file__)), '..', 'data',
         'summary.csv')
     result.to_csv(p, sep="\t")
 
