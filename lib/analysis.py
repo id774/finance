c543effa3361eeec8511f1aec69704f431be0fe1
id774 <idnanashi@gmail.com>
2015-04-28 11:02:16 +0900

    122 Calc NATR

--- lib/analysis.py
+++ lib/analysis.py
@@ -94,6 +94,7 @@ class Analysis():
             ti.calc_macd()
             ti.calc_momentum(timeperiod=10)
             ti.calc_momentum(timeperiod=25)
+            ti.calc_natr()
             vr = ti.calc_volume_ratio()
 
             clf = Classifier(self.clffile)
