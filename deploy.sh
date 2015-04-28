9ffd3e33b50ef814ba30a2be77a8fea8daec5065
id774 <idnanashi@gmail.com>
2015-04-28 10:08:01 +0900

    120 Improvement permissions

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ sudo cp -av run.sh $TARGET_DIR/
 sudo chmod 750 $TARGET_DIR
 sudo chmod 750 $TARGET_DIR/run.sh
 sudo chmod -R g+r,o-rwx $TARGET_DIR
-sudo chown -R root:www-data $TARGET_DIR
-sudo chown -R $USER:www-data $TARGET_DIR/stocks.txt
-sudo chown -R $USER:www-data $TARGET_DIR/clf
+sudo chown -R root:adm $TARGET_DIR
+sudo chown root:www-data $TARGET_DIR
+sudo chown -R $USER:adm $TARGET_DIR/stocks.txt
+sudo chown -R $USER:adm $TARGET_DIR/clf
+sudo chown -R www-data:www-data $TARGET_DIR/data
