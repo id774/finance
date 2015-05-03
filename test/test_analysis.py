19b1010f8f650524be23d333fd3c97044718b112
id774 <idnanashi@gmail.com>
2015-05-04 07:01:54 +0900

    143 Test for analysis

---
+++ test/test_analysis.py
@@ -0,0 +1,58 @@
+import sys
+import os
+from nose.tools import eq_
+p = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '..', 'lib')
+if p in sys.path:
+    pass
+else:
+    sys.path.append(p)
+from ti import TechnicalIndicators
+from analysis import Analysis
+
+def testdata():
+    days = 30
+    filename = os.path.join(os.path.dirname(
+                            os.path.abspath(__file__)),
+                            'stock_N225.csv')
+    stock_tse = pd.read_csv(filename,
+                            index_col=0, parse_dates=True)
+    return stock_tse.asfreq('B')[days:]
+
+def test_run():
+    code='N225'
+    name='日経平均株価'
+    start='2014-01-01'
+    days=180
+    csvfile=os.path.join(os.path.dirname(
+        os.path.abspath(__file__)),
+        '..',
+        'test',
+        'stock_N225.csv')
+    update=False
+
+    analysis = Analysis(code=code,
+                        name=name,
+                        start=start,
+                        days=days,
+                        csvfile=csvfile,
+                        update=update)
+    ti = analysis.run()
+    expected = 18791.39
+    result = round(ti.stock.ix['2015-03-20', 'sma25'], 2)
+    eq_(expected, result)
+
+    filename = 'ti_N225.csv'
+    expected = True
+    eq_(expected, os.path.exists(filename))
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    filename = 'chart_N225.png'
+    expected = True
+    eq_(expected, os.path.exists(filename))
+    if os.path.exists(filename):
+        os.remove(filename)
+
+if __name__ == '__main__':
+    test_run()
