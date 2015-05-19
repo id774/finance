b5175f395b0bd541141a0834464f51e542b710f2
id774 <idnanashi@gmail.com>
2015-05-19 16:56:29 +0900

    159 Range of summary

--- bin/summary.py
+++ bin/summary.py
@@ -8,6 +8,13 @@ else:
     sys.path.append(p)
 from aggregate import Aggregator
 
+def aggregate(aggregator, filename, range=2):
+    result = aggregator.summarize(range=range)
+    p = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), '..', 'data',
+        filename)
+    result.to_csv(p, sep="\t", index_label="Code")
+
 def main():
     c_dir = os.path.dirname(os.path.abspath(__file__))
     base_dir = os.path.join(c_dir, '..')
@@ -16,11 +23,9 @@ def main():
                               'stocks.txt')
 
     aggregator = Aggregator(stock_list, data_dir)
-    result = aggregator.summarize()
-    p = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), '..', 'data',
-        'summary.csv')
-    result.to_csv(p, sep="\t", index_label="Code")
+    aggregate(aggregator, 'summary.csv', range=2)
+    aggregate(aggregator, 'summary_15.csv', range=15)
+    aggregate(aggregator, 'summary_30.csv', range=30)
 
 if __name__ == '__main__':
     argsmin = 0
