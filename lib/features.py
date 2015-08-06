3afba2b561c00f23c2e413f5e8c850bf9e03e3a5
id774 <idnanashi@gmail.com>
2015-08-06 16:40:28 +0900

    206 Test for Regression

--- lib/features.py
+++ lib/features.py
@@ -31,16 +31,6 @@ class Features():
         range = range * -1
         train_X = []
         train_y = []
-        for i in np.arange(range, -15):
-            s = i + 14
-            feature = arr.ix[i:s]
-            if feature[-1] < arr[s]:
-                train_y.append(1)
-            else:
-                train_y.append(0)
-            train_X.append(feature.values)
-        return np.array(train_X), np.array(train_y)
-
         for i in np.arange(range, -14):
             s = i + 14
             feature = arr.ix[i:s]
---
+++ test/test_regression.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import pandas as pd
+from nose.tools import eq_
+p = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '..', 'lib')
+if p in sys.path:
+    pass
+else:
+    sys.path.append(p)
+from ti import TechnicalIndicators
+from regression import Regression
+
+def testdata():
+    days = 91
+    filename = os.path.join(os.path.dirname(
+                            os.path.abspath(__file__)),
+                            'stock_N225.csv')
+    stock_tse = pd.read_csv(filename,
+                            index_col=0, parse_dates=True)
+    return stock_tse.asfreq('B')[days:]
+
+def test_ridge_regression():
+    stock_d = testdata()
+    ti = TechnicalIndicators(stock_d)
+
+    filename = 'test_N225_ridge.pickle'
+    clffile = os.path.join(os.path.dirname(
+                           os.path.abspath(__file__)),
+                           '..', 'clf',
+                           filename)
+
+    if os.path.exists(clffile):
+        os.remove(clffile)
+
+    clf = Regression(filename)
+    ti.calc_ret_index()
+    ret = ti.stock['ret_index']
+    base = ti.stock_raw['Adj Close'][0]
+
+    train_X, train_y = clf.train(ret, regression_type="Ridge")
+
+    test_y = clf.predict(ret, base)
+
+    expected = 19177.97
+    r = round(test_y[0], 2)
+    eq_(r, expected)
+
+    if os.path.exists(clffile):
+        os.remove(clffile)
+
+if __name__ == '__main__':
+    test_ridge_regression()
