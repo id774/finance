d290c9f7275f1ae2f77256aa61856c59bcfc6f4f
id774 <idnanashi@gmail.com>
2015-08-17 17:07:39 +0900

    219 Append tests

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -19,17 +19,19 @@ def testdata():
                             index_col=0, parse_dates=True)
     return stock_tse.asfreq('B')[days:]
 
-def test_run():
-    code = 'N225'
-    name = '日経平均株価'
-    start = '2014-01-01'
-    days = 180
-    csvfile = os.path.join(os.path.dirname(
-        os.path.abspath(__file__)),
-        '..',
-        'test',
-        'stock_N225.csv')
-    update = False
+def test_run(code='N225',
+             name='日経平均株価',
+             start='2014-01-01',
+             days=180,
+             csvfile=os.path.join(os.path.dirname(
+                 os.path.abspath(__file__)),
+                 '..',
+                 'test',
+                 'stock_N225.csv'),
+             update=False,
+             axis=2,
+             complexity=3,
+             reference=[]):
 
     analysis = Analysis(code=code,
                         name=name,
@@ -69,5 +71,11 @@ def test_run():
 
     return result
 
+def test_run3():
+    test_run(axis=1, complexity=1)
+    test_run(axis=2, complexity=2)
+    test_run(axis=2, complexity=3)
+
 if __name__ == '__main__':
     test_run()
+    test_run3()
