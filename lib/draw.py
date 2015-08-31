8ee24ea2fd20148dfd3a614c317710427dc1c5f3
id774 <idnanashi@gmail.com>
2015-08-31 15:19:53 +0900

    227 Swap the top and bottom

--- lib/draw.py
+++ lib/draw.py
@@ -35,74 +35,75 @@ class Draw():
         fig = plt.figure(figsize=(12.80, 10.24))
 
         if axis >= 2:
+            ax1 = fig.add_subplot(2, 1, 1)
+        else:
+            ax1 = fig.add_subplot(1, 1, 1)
+
+        ewma['ewma5'].plot(label="MA5",
+                           color="k", ax=ax1, grid=True)
+        ewma['ewma25'].plot(label="MA25",
+                            color="g", ax=ax1, grid=True)
+        ewma['ewma50'].plot(label="MA50",
+                            color="m", ax=ax1, grid=True)
+        ewma['ewma75'].plot(label="MA75",
+                            color="r", ax=ax1, grid=True)
+        if complexity >= 2:
+            bbands['upperband'].plot(label="UPPER",
+                                     color="c", ax=ax1, grid=True)
+            bbands['lowerband'].plot(label="LOWER",
+                                     color="y", ax=ax1, grid=True)
+        if complexity >= 3:
+            sar['sar'].plot(linestyle=':', label="SAR",
+                            color="#00FF88", ax=ax1, grid=True)
+        if axis == 1:
+            vr['v_rate_p'].plot(linestyle=':', label="VOLUME",
+                                color="#444444", ax=ax1, grid=True)
+        stock_d.plot(kind='ohlc',
+                     colorup='r', colordown='b',
+                     ax=ax1, grid=True)
+        ncol = complexity + 1
+        if axis >= 2:
+            plt.legend(loc='upper center', bbox_to_anchor=(0.36, 1.228),
+                       ncol=ncol, fancybox=False, shadow=False)
+
             roc['roc10'] = roc['roc10'] + 50
             roc['roc25'] = roc['roc25'] + 50
             willr['willr14'] = willr['willr14'] + 100
             tr['vl'] = tr['vl'] * 5
 
-            ax1 = fig.add_subplot(2, 1, 1)
+            ax2 = fig.add_subplot(2, 1, 2)
             rsi['rsi9'].plot(label="RSI9",
-                             color="g", ax=ax1)
+                             color="g", ax=ax2)
             rsi['rsi14'].plot(label="RSI14",
-                              color="r", ax=ax1)
+                              color="r", ax=ax2)
             roc['roc10'].plot(label="ROC10",
-                              color="b", ax=ax1)
+                              color="b", ax=ax2)
             roc['roc25'].plot(label="ROC25",
-                              color="#888888", ax=ax1, grid=True)
+                              color="#888888", ax=ax2, grid=True)
             if complexity >= 3:
                 mfi['mfi14'].plot(label="MFI",
-                                  color="#DD88DD", ax=ax1, grid=True)
+                                  color="#DD88DD", ax=ax2, grid=True)
                 ultosc['ultosc'].plot(label="UO",
-                                      color="m", ax=ax1, grid=True)
+                                      color="m", ax=ax2, grid=True)
             if complexity >= 2:
                 stoch['slowk'].plot(label="SLOWK",
-                                    color="y", ax=ax1, grid=True)
+                                    color="y", ax=ax2, grid=True)
                 stoch['slowd'].plot(label="SLOWD",
-                                    color="k", ax=ax1, grid=True)
+                                    color="k", ax=ax2, grid=True)
             if complexity >= 3:
                 willr['willr14'].plot(linestyle=':', label="%R",
-                                      color="#FF0088", ax=ax1, grid=True)
+                                      color="#FF0088", ax=ax2, grid=True)
             tr['vl'].plot(label="VL",
-                          color="c", ax=ax1, grid=True)
+                          color="c", ax=ax2, grid=True)
             vr['v_rate'].plot(label="VOLUME", kind='area',
-                              color="#DDFFFF", ax=ax1, grid=True)
+                              color="#DDFFFF", ax=ax2, grid=True)
             if len(ref) > 0:
                 self.ref_result = (" 日経相関:" +
                                    str(round(ref.mean(), 2)))
                 ref = ref * 50 + 50
                 ref.plot(linestyle=':', label="REF",
-                         color="#DDDDDD", ax=ax1, grid=True)
-            ax1.set_yticks([0, 25, 50, 75, 100])
-            ncol = complexity + 3
-            plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.23),
-                       ncol=ncol, fancybox=False, shadow=False)
-
-            ax2 = fig.add_subplot(2, 1, 2)
-        else:
-            ax2 = fig.add_subplot(1, 1, 1)
-
-        ewma['ewma5'].plot(label="MA5",
-                           color="k", ax=ax2, grid=True)
-        ewma['ewma25'].plot(label="MA25",
-                            color="g", ax=ax2, grid=True)
-        ewma['ewma50'].plot(label="MA50",
-                            color="m", ax=ax2, grid=True)
-        ewma['ewma75'].plot(label="MA75",
-                            color="r", ax=ax2, grid=True)
-        if complexity >= 2:
-            bbands['upperband'].plot(label="UPPER",
-                                     color="c", ax=ax2, grid=True)
-            bbands['lowerband'].plot(label="LOWER",
-                                     color="y", ax=ax2, grid=True)
-        if complexity >= 3:
-            sar['sar'].plot(linestyle=':', label="SAR",
-                            color="#00FF88", ax=ax2, grid=True)
-        if axis == 1:
-            vr['v_rate_p'].plot(linestyle=':', label="VOLUME",
-                                color="#444444", ax=ax2, grid=True)
-        stock_d.plot(kind='ohlc',
-                     colorup='r', colordown='b',
-                     ax=ax2, grid=True)
+                         color="#DDDDDD", ax=ax2, grid=True)
+            ax2.set_yticks([0, 25, 50, 75, 100])
 
         _close = int(stock_d.ix[-1, 'Adj Close'])
         _last_close = int(stock_d.ix[-2, 'Adj Close'])
@@ -135,12 +136,13 @@ class Draw():
                     '{:,d}'.format(reg_result),
                     ]),
                    fontdict={"fontproperties": self.fontprop})
-        ncol = complexity + 1
+        ncol = complexity + 2
         if axis >= 2:
-            plt.legend(loc='upper center', bbox_to_anchor=(0.367, 1.228),
+            plt.legend(loc='upper center', bbox_to_anchor=(0.48, 1.23),
                        ncol=ncol, fancybox=False, shadow=False)
         else:
-            plt.legend(loc='upper center', bbox_to_anchor=(0.38, 1.12),
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.105),
                        ncol=ncol, fancybox=False, shadow=False)
+
         plt.savefig("".join(["chart_", self.code, ".png"]))
         plt.close()
