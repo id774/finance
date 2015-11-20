ff5b25e8d59ea215dba9bf85626ba5c2cb863eb5
id774 <idnanashi@gmail.com>
2015-11-20 13:52:44 +0900

    236 Send screening report

--- run.sh
+++ run.sh
@@ -19,10 +19,12 @@ $PYTHON $WORK_DIR/bin/summary.py -o summary_10.csv -r 10 -k Ratio>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -o summary_25.csv -r 25 -k Ratio>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -o rolling_corr.csv -r 1 -k Corr>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -s my_stocks.txt -o portfolio.csv -r 1 -k Ratio>>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/summary.py -o screening_rsi9.csv -r 1 -c rsi9 -a -k rsi9>>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/reference.rb>>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/email.rb>>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/email.rb "rolling_corr.csv" "Summary Report sorted by corr">>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/email.rb "portfolio.csv" "Summary Report of your Portfolio">>$JOBLOG 2>&1
+$RUBY $WORK_DIR/bin/email.rb "screening_rsi9.csv" "Screening Report by RSI9">>$JOBLOG 2>&1
 
 echo -n "*** $0: Job ended on `/bin/hostname` at ">>$JOBLOG 2>&1
 date "+%Y/%m/%d %T">>$JOBLOG 2>&1
