40c97756996d5fd1dd3eb81da9dcd499782c733f
id774 <idnanashi@gmail.com>
2016-12-30 10:36:37 +0900

    290 Create long term technical indicators

--- update_N225.sh
+++ update_N225.sh
@@ -5,15 +5,14 @@ WORK_DIR=/var/stock
 RUBY=/opt/ruby/current/bin/ruby
 PYTHON=/opt/python/current/bin/python
 STOCKTXT=$WORK_DIR/data/stocks.txt
-STARTDATE=2014-10-01
-DAYS=240
 
 cd $WORK_DIR/data
 
 echo -n "*** $0: Job started on `/bin/hostname` at ">>$JOBLOG 2>&1
 date "+%Y/%m/%d %T">>$JOBLOG 2>&1
 
-$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 3 -c N225 -n 日経平均株価 -d 2015-01-01 -y 180 -r stock_N225.csv -u>>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/charts.py -a 1 -p 4 -c N225 -n 日経平均株価 -d 2014-01-01 -y 600 -r stock_N225.csv -u>>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 3 -c N225 -n 日経平均株価 -d 2014-01-01 -y 240 -r stock_N225.csv>>$JOBLOG 2>&1
 
 echo -n "*** $0: Job ended on `/bin/hostname` at ">>$JOBLOG 2>&1
 date "+%Y/%m/%d %T">>$JOBLOG 2>&1
