cadb03d7ef9ebc74a5717fa6b28c914599aeac3b
id774 <idnanashi@gmail.com>
2018-02-02 10:44:49 +0900

    306 Improvement execute sequence

--- run.sh
+++ run.sh
@@ -15,9 +15,7 @@ cd $WORK_DIR/data
 echo -n "*** $0: Job started on `/bin/hostname` at ">>$JOBLOG 2>&1
 date "+%Y/%m/%d %T">>$JOBLOG 2>&1
 
-$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 1 -s $STOCKTXT -d $STARTDATE -y $LONGDAYS -u>>$JOBLOG 2>&1
-$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 3 -s $STOCKTXT -d $STARTDATE -y $SHORTDAYS>>$JOBLOG 2>&1
-$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 2 -s $STOCKTXT -d $STARTDATE -y $DAYS>>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 2 -s $STOCKTXT -d $STARTDATE -y $DAYS -u>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -o summary.csv -y -r 1 -k Ratio>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -o summary_10.csv -r 10 -k Ratio>>$JOBLOG 2>&1
 $PYTHON $WORK_DIR/bin/summary.py -s my_stocks.txt -o portfolio.csv -r 1 -k Ratio>>$JOBLOG 2>&1
@@ -27,6 +25,8 @@ $RUBY $WORK_DIR/bin/reference.rb>>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/email.rb>>$JOBLOG 2>&1
 # $RUBY $WORK_DIR/bin/email.rb "rolling_corr.csv" "Summary Report sorted by corr">>$JOBLOG 2>&1
 $RUBY $WORK_DIR/bin/email.rb "portfolio.csv" "Summary Report of My Portfolio">>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 1 -s $STOCKTXT -d $STARTDATE -y $LONGDAYS>>$JOBLOG 2>&1
+$PYTHON $WORK_DIR/bin/charts.py -a 2 -p 3 -s $STOCKTXT -d $STARTDATE -y $SHORTDAYS>>$JOBLOG 2>&1
 
 echo -n "*** $0: Job ended on `/bin/hostname` at ">>$JOBLOG 2>&1
 date "+%Y/%m/%d %T">>$JOBLOG 2>&1
