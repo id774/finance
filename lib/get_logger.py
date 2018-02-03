33307c2cff91e617eeb0ff0a41f869b871a18caa
id774 <idnanashi@gmail.com>
2018-02-03 11:25:18 +0900

    317 Show nano-second

--- lib/get_logger.py
+++ lib/get_logger.py
@@ -4,15 +4,15 @@ class Logger():
 
     def info(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
-        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
         print(now, "INFO", message)
 
     def warn(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
-        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
         print(now, "WARN", message)
 
     def error(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
-        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+        now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
         print(now, "ERROR", message)
