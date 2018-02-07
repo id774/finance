0840393c17a3a9bf94db339cef59163a182d464f
id774 <idnanashi@gmail.com>
2018-02-07 13:44:59 +0900

    318 Shaping logs

--- lib/get_logger.py
+++ lib/get_logger.py
@@ -5,14 +5,14 @@ class Logger():
     def info(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
         now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
-        print(now, "INFO", message)
+        print(now, "[INFO]", message)
 
     def warn(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
         now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
-        print(now, "WARN", message)
+        print(now, "[WARN]", message)
 
     def error(self, message):
         JST = timezone(timedelta(hours=+9), 'JST')
         now = datetime.now(JST).strftime("%Y-%m-%dT%H:%M:%S.%s+09:00")
-        print(now, "ERROR", message)
+        print(now, "[ERROR]", message)
